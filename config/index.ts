4d043bf884acc3bcce585b4549ed5e652971bb5d
puluter <pkmq24@qq.com>
2023-06-09 09:28:35 +0800

    ?

--- config/index.ts
+++ config/index.ts
@@ -1,16 +1,16 @@
-import { AppInfo } from "@/types/app"
-export const APP_ID = ''
-export const API_KEY = ''
+import type { AppInfo } from '@/types/app'
+export const APP_ID = '99f69357-bf03-4ccb-8566-643d4c3307ef'
+export const API_KEY = 'app-CDjxVtFQUIvzZCywBlPKkQzt'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  "title": 'Text Generator APP',
-  "description": 'App description',
-  "copyright": '',
-  "privacy_policy": '',
-  "default_language": 'zh-Hans'
+  title: 'Python帮帮我',
+  description: '自动生成python题目代码的错误修复。',
+  copyright: '',
+  privacy_policy: '',
+  default_language: 'zh-Hans',
 }
 
-export const API_PREFIX = '/api';
+export const API_PREFIX = '/api'
 
 export const LOCALE_COOKIE_NAME = 'locale'
 
